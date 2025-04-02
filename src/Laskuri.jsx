33ba72a1cb689b2ce933df94cc6acfd500d4b89f
Simppa <simo.siren@careeria.fi>
2025-04-02 11:37:12 +0300

    Laskuri plussa toimii

--- src/Laskuri.jsx
+++ src/Laskuri.jsx
@@ -1,12 +1,21 @@
 import './App.css'
+import React from 'react'
 
 
 function Laskuri() {
+
+// Tämän komponentin state on luku.
+// Set lukua kutsumalla lukua voi muuttaa.
+// nolla lopussa on luku staten alkuarvo
+const [luku, setLuku] = React.useState(0)
 
   return (
     <>
 
      <h2>Laskuri</h2>
+
+     <h3>{luku}</h3>
+     <button onClick={() => setLuku(luku + 1)}>+</button>
 
     </>
   )
